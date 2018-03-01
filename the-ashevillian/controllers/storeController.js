a2e4b3e714842f7fcf3e7060eb68d58f4e2fcfef
Morgan Murrah <mmurrah@airbridge.ac.nz>
2018-03-01 18:59:44 -0500

    Controller update; Get Stores

--- the-ashevillian/controllers/storeController.js
+++ the-ashevillian/controllers/storeController.js
@@ -15,3 +15,9 @@ exports.createStore = async(req, res) => {
     req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  // 1. Query Database for List of All Stores
+  const stores = await Store.find();
+  res.render('stores', {title: 'stores', stores });
+};
