dd12ed41f2300b2b121791c3f5d27b91778bee6d
Morgan Murrah <mmurrah@airbridge.ac.nz>
2018-05-04 21:11:35 -0400

    Account view

--- the-ashevillian/routes/index.js
+++ the-ashevillian/routes/index.js
@@ -48,6 +48,8 @@ router.post('/register',
 
 router.get('/logout', authController.logout);
 
-router.get('/account', userController.account);
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', catchErrors(userController.updateAccount));
+
 
 module.exports = router;
