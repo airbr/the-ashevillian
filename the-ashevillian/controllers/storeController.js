2ee422438958d732ba9f563d5b4be4858b1f19f1
Morgan Murrah <mmurrah@airbridge.ac.nz>
2018-05-20 12:27:08 -0400

    Add Pagination

--- the-ashevillian/controllers/storeController.js
+++ the-ashevillian/controllers/storeController.js
@@ -59,9 +59,29 @@ exports.createStore = async(req, res) => {
 };
 
 exports.getStores = async (req, res) => {
+  const page = req.params.page || 1;
+  const limit = 12;
+  const skip = (page * limit) - limit;
   // 1. Query Database for List of All Stores
-  const stores = await Store.find();
-  res.render('stores', {title: 'stores', stores });
+  const storesPromise = Store
+      .find()
+      .skip(skip)
+      .limit(limit)
+      .sort({ created: 'desc'});
+
+  const countPromise = Store.count();
+
+  const [stores, count] = await Promise.all([storesPromise, countPromise]);
+
+  const pages = Math.ceil(count / limit);
+
+  if (!stores.length && skip) {
+    req.flash('info', `Hey! You asked for page ${page}. But that doesn't exist. So I put you on page ${pages}`);
+    res.redirect(`/stores/page/${pages}`);
+    return;
+  }
+
+  res.render('stores', {title: 'Stores', stores, page, pages, count });
 };
 
 const confirmOwner = (store, user) => {
