00d43fd529e3fcc1198bea118f67d1e8083b7f44
Morgan Murrah <mmurrah@airbridge.ac.nz>
2018-05-23 18:43:56 -0400

    Remove Stats controller

--- the-ashevillian/routes/index.js
+++ the-ashevillian/routes/index.js
@@ -31,8 +31,6 @@ router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
-router.get('/stats', catchErrors(statsController.showStats));
-
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
